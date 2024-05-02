b7b48f55b223e36156a6e90496fb9a1d33d177a8
Amogh2513 <amoghjainapur@gmail.com>
2024-05-02 20:56:06 +0530

    update

--- c.java
+++ c.java
@@ -2,6 +2,10 @@ public class c {
 
 	public static void main(String[] args) {
 		System.out.println("hello welcome to gpt");
+        System.out.println("hello welcome to gpt");
+        System.out.println("hello welcome to gpt");
+        System.out.println("hello welcome to gpt");
+        System.out.println("hello welcome hfjgh");
 
 	}
 
